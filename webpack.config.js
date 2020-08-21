dcd63f72f37a707d9f641988091f44a8de4775f2
Sinclair Target <sinclairtarget@gmail.com>
2020-08-21 14:42:44 -0400

    Add nice font styles.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader']
+        use: [ 'style-loader', 'css-loader?url=false', 'sass-loader']
       }
     ]
   },
