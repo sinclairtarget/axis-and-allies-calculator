70f6682dafaad1d047902e11536e4a16998778b5
Sinclair Target <sinclairtarget@gmail.com>
2020-09-13 17:08:02 -0400

    Note text for ExpectedLoss component is specific to attacker/defender.

--- src/components/simulation-review/ExpectedLoss.js
+++ src/components/simulation-review/ExpectedLoss.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
+import { ATTACKER_KEY, DEFENDER_KEY } from '../../lib/simulation-results.js';
+
 import './ExpectedLoss.scss';
 
 let format = d3.format('.1f');
+
+function noteText(vizKey) {
+  let side = vizKey == ATTACKER_KEY ? 'the attacker' : 'the defender';
+  return `This is the value of the units ${side} would lose on average
+          if you were to fight this battle over and over again.`;
+}
 
 function ExpectedLoss(props) {
   let losses = props.simulation
@@ -14,8 +22,7 @@ function ExpectedLoss(props) {
   return (
     <div className="ExpectedLoss">
       <h1 className="big-number">{format(mean)}</h1>
-      <p className="note">This is the value of the units this side would lose on
-    average if you were to fight this battle over and over again.</p>
+      <p className="note">{noteText(props.vizKey)}</p>
     </div>
   );
 };
